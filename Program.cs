21b63d6a7d13d130fa93947d523a5c9404e6b263
Winebug <ydjoo0410@gmail.com>
2025-07-10 20:27:17 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,21 +4,35 @@ class Status
 {
     public void StatusState(Character character)
     {
+        float equippedAttack = 0;
+        float equippedDefense = 0;
+
+        foreach (Item item in character.inventory)
+        {
+            if (item.IsEquipped)
+            {
+                if (item.Type == "공격력") equippedAttack += item.Value;
+                if (item.Type == "방어력") equippedDefense += item.Value;
+            }
+        }
+
         Console.Clear();
         Console.WriteLine("상태 보기");
         Console.WriteLine("캐릭터의 정보가 표시됩니다.");
         Console.WriteLine();
-        Console.WriteLine($"Lv. {character.level}");
+        Console.WriteLine($"[Lv. {character.level}]");
         Console.WriteLine($"{character.name} ({character.job})");
-        Console.WriteLine($"공격력 : {character.attack}+");
-        Console.WriteLine($"방어력 : {character.defense}");
+        Console.WriteLine($"공격력 : {character.attack}  (스텟:{character.attack - equippedAttack} + (무기:{equippedAttack})");
+        Console.WriteLine($"방어력 : {character.defense}   (스텟:{character.defense - equippedDefense}  + (갑옷:{equippedDefense})");
         Console.WriteLine($"체  력 : {character.hp}");
-        Console.WriteLine($"Gold  : {character.gold}G");
+        Console.WriteLine($"Gold   : {character.gold}G");
         Console.WriteLine();
         Console.WriteLine("0. 나가기");
         Console.WriteLine();
         Console.WriteLine("원하시는 행동을 입력해주세요.");
         Console.Write(">> ");
+
+
     }
 }
   
@@ -34,15 +48,17 @@ class Character
 
     public List<Item> inventory = new List<Item>();
 
+
+
     public Character()
     {
         level = 1;
-        name = "Eat-Dong";
-        job = "전사";
+        name = "곽철이";
+        job = "꽉꽉도적";
         attack = 10;
         defense = 5;
         hp = 100;
-        gold = 1500;
+        gold = 1500000000;
     }
 }
 
@@ -99,7 +115,8 @@ class Program
             new Item("[고급]", "스파르타의 갑옷", 15, "방어력", " 갑옷",  "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.",3500),
             new Item("[일반]", "낡은 검        ", 2,  "공격력", " 무기",  "쉽게 볼 수 있는 낡은 검 입니다.                  ",600),
             new Item("[일반]", "청동 도끼      ", 5,  "공격력", " 무기",  "어디선가 사용됐던거 같은 도끼입니다.             ",1500),
-            new Item("[에픽]", "구원의 이기    ", 680,"공격력", " 무기",  "던붕이들은 아는 그런 무기입니다.                 ",1000000)
+            new Item("[에픽]", "진명황의 집판점", 777,"공격력", " 무기",  "던붕이들은 아는 그런 무기입니다.                 ",1000000),
+            new Item("[에픽]", "진명황의 갑옷  ", 777,"방어력", " 갑옷",  "던붕이들은 아는 그런 무기입니다.                 ",1000000)
         };
 
         bool GameManager = true;
@@ -113,6 +130,8 @@ class Program
             Console.WriteLine("1. 상태 보기");
             Console.WriteLine("2. 인벤토리");
             Console.WriteLine("3. 상점");
+            Console.WriteLine("4. 던전입장");
+            Console.WriteLine("5. 휴식하기");
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
             Console.Write(">> ");
@@ -134,7 +153,7 @@ class Program
                             Console.ReadKey();
                         }
                     }
-                    break;
+                    break; 
 
                 case "2":
                     bool inInventory = true;
@@ -155,7 +174,7 @@ class Program
                             {
                                 Item item = character.inventory[i];
                                 string equippedMark = item.IsEquipped ? "[E]" : "   ";
-                                Console.WriteLine($"- {equippedMark}{item.Name.Trim()} | {item.Type,-6} +{item.Value,2} | {item.Description.Trim()}");
+                                Console.WriteLine($"- {equippedMark}{item.Name} | {item.Type} +{item.Value} | {item.Description}");
                             }
                         }
                         Console.WriteLine();
@@ -169,7 +188,19 @@ class Program
                         if (equipInput == "0") inInventory = false;
                         else if (equipInput == "1")
                         {
-                            Console.WriteLine("장착할 아이템 번호를 입력해주세요 (1 ~ N) >> ");
+                            Console.Clear();
+                            Console.WriteLine("장착 가능한 아이템 목록");
+                            Console.WriteLine();
+
+                            for (int i = 0; i < character.inventory.Count; i++)
+                            {
+                                Item item = character.inventory[i];
+                                string equippedMark = item.IsEquipped ? "[E]" : "   ";
+                                Console.WriteLine($"{i + 1}. {equippedMark}{item.Name} | {item.Type} +{item.Value} | {item.Description}");
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine("장착할 아이템 번호를 입력해주세요 (1 ~ N, 0. 취소) >> ");
                             string numInput = Console.ReadLine();
 
                             if (int.TryParse(numInput, out int idx) && idx >= 1 && idx <= character.inventory.Count)
@@ -190,8 +221,13 @@ class Program
                                 if (selectedItem.Type == "공격력") character.attack += selectedItem.Value;
                                 if (selectedItem.Type == "방어력") character.defense += selectedItem.Value;
 
+                                Console.WriteLine();
                                 Console.WriteLine($"{selectedItem.Name.Trim()}을(를) 장착했습니다!");
                                 Console.ReadKey();
+                            }
+                            else if (idx == 0)
+                            {
+                                
                             }
                             else
                             {
@@ -278,14 +314,106 @@ class Program
                     }
                     break;
 
+                case "4":
+                    {
+                        bool Dun = true;
+                        while (Dun)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("던전입장");
+                            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+                            Console.WriteLine();
+                            Console.WriteLine("1. 쉬운 던전     | 방어력 5이상 권장");
+                            Console.WriteLine("2. 일반 던전     | 방어력 11이상 권장");
+                            Console.WriteLine("3. 어려운 던전   | 방어력 17이상 권장");
+                            Console.WriteLine("0. 나가기");
+                            Console.WriteLine();
+                            Console.WriteLine("원하시는 행동을 입력해주세요.");
+                            Console.Write(">> ");
+                            string sub = Console.ReadLine();
+
+                            if (sub == "0") Dun = false;
+                            else if (sub == "1")
+                            {
+
+                            }
+                            else if (sub == "2")
+                            {
+
+                            }
+                            else if (sub == "3")
+                            {
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("잘못된 입력입니다.");
+                                Console.ReadKey();
+                            }
+                        }
+                    }
+                    break;
+
+                case "5":
+                    {
+                        bool rest = true;
+                        while (rest)
+                        {
+                            Console.Clear();
+                            Console.WriteLine();
+                            Console.WriteLine("휴식하기");
+                            Console.WriteLine($"500G를 내면 체력을 회복할 수 있습니다. (보유 골드 : {character.gold}G)");
+                            Console.WriteLine();
+                            Console.WriteLine("1. 휴식하기");
+                            Console.WriteLine("0. 나가기");
+                            Console.WriteLine();
+                            Console.WriteLine("원하시는 행동을 입력해주세요.");
+                            Console.Write(">> ");
+
+                            string sub = Console.ReadLine();
+
+                            if (sub == "0")
+                            {
+                                rest = false;
+                            }
+                            else if (sub == "1")
+                            {
+                                if (character.gold >= 500)
+                                {
+                                    int temp_hp = 0;
+
+                                    temp_hp = (100 - character.hp);
+                                    character.hp += temp_hp;
+
+                                    character.gold -= 500;
+                                    Console.WriteLine();
+                                    Console.WriteLine("휴식을 완료했습니다.");
+                                    Console.WriteLine($"현재 체력은 {character.hp}입니다.");
+                                    Console.WriteLine();
+                                    Console.ReadKey();
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("Gold가 부족합니다.");
+                                    Console.WriteLine();
+                                    Console.ReadKey();
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("잘못된 입력입니다");
+                            }
+                        }
+                    }
+                    break;
+
                 default:
                     Console.WriteLine("잘못된 입력입니다.");
                     Console.ReadKey();
                     break;
             }
         }
-
-        Console.Clear();
-        Console.WriteLine("게임을 종료합니다. 안녕히 가세요!");
     }
 }
